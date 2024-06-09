232d065369e7d14ef838f71f08b24c1a58559252
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-06-08 19:40:34 -0500

    (Backend): Definición de las reglas del lenguaje para tipos de datos más complejos como numeros, strings, variables y comentarios

---
+++ lexer.py
@@ -0,0 +1,85 @@
+import ply.lex as lex
+from tokens import *
+
+# Float numbers
+def t_FLOATNUMBER(t):
+    r'(\d*\.\d+|\d+\.\d*)'
+    t.value = float(t.value)
+    return t
+
+# Integer numbers
+def t_INTNUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Comment
+def t_COMMENT(t):
+    r'\#\#\#.*'
+    t.value = f"{t}"
+    return t
+
+# Varible
+def t_VAR(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'VAR')   
+    return t
+
+# Strings
+def t_STRING(t):
+    r'(\'[^\']*\'|\"[^\"]*\")'
+    t.value = str(t.value[1:-1])
+    return t
+
+
+# Next Line
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# Test
+data = '''
+
+22
+1.5 .5
++ - * /
+()
+{}
+==
+>=
+<=
+<!>
+=
+condition
+otherwise_if
+otherwise
+during
+make
+cycle
+and
+or
+not
+miVariable
+mi_variable
+miVar
+"Tremendo"
+process
+'''
+
+# Data input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break  
+    print(f"Type: {tok.type} | Value: {tok.value} | Line: {tok.lineno} | Position: {tok.lexpos}")
