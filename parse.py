838184e9c1c00613cd6627beefc8e993cc3e3327
Jonathan Vizcaino <jevizcaino@unimagdalena.edu.co>
2024-06-09 14:46:19 -0500

    Modificación de parser para compuertas logicas

--- parse.py
+++ parse.py
@@ -10,21 +10,33 @@ def p_binary_operators(p):
                   | term DIVIDE factor'''  
     if isinstance(p[1], (int,str,float)) and isinstance(p[3], (int,str,float)):
         if p[2] == '+':
-            p[0] = p[1] + p[3]
+
+            if isinstance(p[1], (int, float)) and isinstance(p[3], (int, float)):
+                p[0] = p[1] + p[3]
+            elif isinstance(p[1], str) and isinstance(p[3], str):
+                p[0] = p[1] + p[3]
+            else:
+                print("Cant only concatenate string to string")
+
         elif p[2] == '-':
             if not isinstance(p[1], str) and not isinstance(p[3], str):
                 p[0] = p[1] - p[3]
             else:
                 print("unsupported operation")
         elif p[2] == '*':
-            p[0] = p[1] * p[3]
+            if isinstance(p[1], (int, float)) and isinstance(p[3], (int, float)):
+                p[0] = p[1] * p[3]
+            elif isinstance(p[1], int) and isinstance(p[3], str):
+                p[0] = p[1] * p[3]
+            elif isinstance(p[1], str) and isinstance(p[3], int):
+                p[0] = p[1] * p[3]
+            else:
+                print("can't multiply sequence by non-int of type 'float'")
         elif p[2] == '/':
             if not isinstance(p[1], str) and not isinstance(p[3], str):
                 p[0] = p[1] / p[3]
             else:
                 print("unsupported operation")
-
-
 
 def p_factor_var(p):
     'factor : VAR'
@@ -43,6 +55,29 @@ def p_factor_boolean(p):
     '''factor : TRUE
               | FALSE'''
     p[0] = p[1]
+
+def p_expression_and(p):
+    'expression : expression AND expression'
+    if isinstance(p[1], (str, int, float)) and isinstance(p[3], (str, int, float)):
+        if(p[1]=='Truth' and p[3]=='Truth'):
+            p[0] = 'Truth'
+        elif(p[1]=='Truth' and p[3]=='Lie' or p[1]=='Lie' and p[3]=='Truth'):
+            p[0] = 'Lie'
+        else:
+            if(p[1] == 'Lie' or p[3] == 'Lie'):
+                p[0] = 'Lie'
+            else:
+                p[0] = p[3]
+
+def p_expression_or(p):
+    'expression : expression OR expression'
+    if isinstance(p[1], (str, int, float)) and isinstance(p[3], (str, int, float)):
+        if(p[1]=='Lie' and p[3]=='Lie'):
+            p[0] = 'Lie'
+        elif(p[1]=='Truth' and p[3]=='Lie' or p[1]=='Lie' and p[3]=='Truth'):
+            p[0] = 'Truth'
+        else:
+            p[0] = p[3]
 
 def p_expression_comparison(p):
     '''expression : expression EQUALSEQUALS expression
@@ -88,6 +123,18 @@ def p_term_factor(p):
     'term : factor'
     p[0] = p[1]
 
+def p_factor_and(p):
+    'factor : AND'
+    p[0] = p[1]
+
+def p_factor_or(p):
+    'factor : OR'
+    p[0] = p[1]
+
+def p_factor_not(p):
+    'factor : NOT'
+    p[0] = p[1]
+
 def p_factor_intnum(p):
     'factor : INTNUMBER'
     p[0] = p[1]
@@ -128,17 +175,11 @@ def p_error(p):
 # Build the parser
 parser = yacc.yacc()
 
-s = '''
-show("Hola Cachón") 
-a = 1+2
-show(a)
-'''
-
-
-lines = s.splitlines()
-
-for line in lines:
-    if line.strip(): 
-        result = parser.parse(line)
-        if result is not None:
-            print(result)
+while True:
+   try:
+       s = input('\nvazritch > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
